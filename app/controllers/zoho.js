146e8daef2d8d54ebf219aecac2f167b83d449c2
George <jordgo1095@gmail.com>
2019-02-20 13:37:29 +0400

    17

--- app/controllers/zoho.js
+++ app/controllers/zoho.js
@@ -126,7 +126,7 @@ exports.authRefresh = function(req, res, next){
         request.post(url, function(err, response, body){
             if(err) console.error('//////////////// error post refresh //////////////////////');
             //console.log(body);
-            console.log('--------------- ' +response);
+            console.log(response);
             console.log('00000000000000000000000000000000000000000000');
             
         console.log(body.access_token);
