6e79528f494f136deb62e80b9dda40036d04dbc3
George <jordgo1095@gmail.com>
2019-02-20 11:19:31 +0400

    8

--- config/routes.js
+++ config/routes.js
@@ -40,7 +40,7 @@ module.exports = function (app, passport, bot) {
 	// app.get('/auth/getcode*', function(req, res){
 	// 	res.render('auth')
 	// })
-	app.get('/auth/getcode*', zohos.authRefresh(req, res, next), function(req, res){
+	app.get('/auth/getcode*', zohos.authRefresh, function(req, res){
 		res.render('auth')
 	})
 	app.post('/auth/saveclient', zohos.authGetAuthCode)
