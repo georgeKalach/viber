d391e084213e482a86130a66d59f757f43506138
George <jordgo1095@gmail.com>
2019-02-28 10:03:37 +0400

    zoho deploy

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,7 @@
 const DEFAULT_USER_EMAIL = 'example9@gmail.com'
 const DEFAULT_USER_PASSWORD = '1234567'
 const VIBER_AUTH = '494391b83227d1e8-2838b1e9fc30879f-3379fecf51f52ea'//'492dd39cdd67d7e9-8183cf8aa72f5f83-4b9561e01920061f'
-const URL_ZOHO = 'https://desk.zoho.com/support/enovateit';//'http://localhost:5000'
+const URL_ZOHO = 'https://336fe107.ngrok.io'//'https://desk.zoho.com/support/enovateit';//'http://localhost:5000'
 
 module.exports = { 
     DEFAULT_USER_EMAIL: DEFAULT_USER_EMAIL,
