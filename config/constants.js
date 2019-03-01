46365a610d62a4b2d190ff201b5c04289c007880
George <jordgo1095@gmail.com>
2019-03-01 09:14:14 +0400

    zoho deploy6

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,7 @@
 const DEFAULT_USER_EMAIL = 'example9@gmail.com'
 const DEFAULT_USER_PASSWORD = '1234567'
 const VIBER_AUTH = '494391b83227d1e8-2838b1e9fc30879f-3379fecf51f52ea'//'492dd39cdd67d7e9-8183cf8aa72f5f83-4b9561e01920061f'
-const URL_ZOHO = 'https://desk.zoho.com/support/enovateit/ShowHomePage.do';//'https://desk.zoho.com/support/enovateit';//'http://localhost:5000'
+const URL_ZOHO = 'https://67000001216557.zappsusercontent.com/appfiles/67000001216557/4.0/5cf92f90ff6480b5080e5b10c89edcf7ad1896d10d18c6b6a57a3a5b2c9bcb80/app/widget.html';//'https://desk.zoho.com/support/enovateit';//'http://localhost:5000'
 
 module.exports = { 
     DEFAULT_USER_EMAIL: DEFAULT_USER_EMAIL,
