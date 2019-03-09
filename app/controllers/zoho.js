2147c97c4548587dc5e06f0bb222933fb02e21a2
George <jordgo1095@gmail.com>
2019-03-09 21:37:51 +0400

    support5

--- app/controllers/zoho.js
+++ app/controllers/zoho.js
@@ -231,7 +231,7 @@ exports.authRefresh = function(req, res, next){
         let redirect = 'https://damp-tundra-61257.herokuapp.com'
         let params = `?code=${code}&grant_type=authorization_code&client_id=${client_id}&client_secret=${client_secret}&redirect_uri=${redirect}&scope=${scope}`;
 
-        var url = `https://accounts.zoho.com/oauth/v2/auth${params}`;
+        var url = `https://accounts.zoho.com/oauth/v2/token${params}`;
 
         request.post(url,  function(err, body, response){
             if(err) console.error('////// error post refresh /////////'+err);
